a1006776424941f3c26007c40776e847873a1306
Wil220 <wilker.silva0714@gmail.com>
2024-06-04 19:11:37 -0300

    calcule e mostre a diferença do produto entre o n1 e n2 pelo produto entre o n3 e o n4

---
+++ atividade4/Atividade4.java
@@ -0,0 +1,32 @@
+package atividade4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Atividade4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	    float n1, n2, n3, n4, calculo;
+	    
+		System.out.println("Número 1 ");
+		n1 = sc.nextFloat();
+		
+		System.out.println("número 2 ");
+		n2 = sc.nextFloat();
+		
+		System.out.println("número 3 ");
+		n3 = sc.nextFloat();
+		
+		System.out.println("número 4 ");
+		n4 = sc.nextFloat();
+		
+		calculo = (n1 * n2) - (n3 * n4);
+		
+		System.out.printf("Diferença %.2f", calculo);	
+	sc.close();	
+	}
+}
