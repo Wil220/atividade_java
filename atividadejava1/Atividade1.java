33276e8b1d6d3ea92ec6aef1db63b21bdf6253d4
Wil220 <wilker.silva0714@gmail.com>
2024-06-04 15:09:29 -0300

    atividade1

---
+++ atividadejava1/Atividade1.java
@@ -0,0 +1,27 @@
+package atividadejava1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		float salario;
+		float abono;
+		float novoSalario;
+		
+		System.out.println("Digite seu salario");
+		salario = sc.nextFloat();
+		System.out.println("Digite o Abono");
+		abono = sc.nextFloat();
+		novoSalario = salario + abono;
+		System.out.printf("seu novo salario é %.2f", novoSalario);
+
+		sc.close();
+
+	}
+
+}
